5fc4700de9033e751347bbee65826d04abc8fc84
ADK <artemdk7@gmail.com>
2022-08-07 21:51:02 +0700

    task 1 and2 complete

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,4 +6,17 @@
 //       a = -3 b = 9 -> нет
 
 
-Console.WriteLine("Hello, World!");
+        Console.Write("Введите целое число 1: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите целое число 2: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+   if (num1 == num2 * num2)
+ {
+             Console.Write($"число {num1} является квадратом числа {num2}");
+ }        
+ else 
+        { 
+             Console.Write($"число {num1} не является квадратом числа {num2}");
+         }
+
