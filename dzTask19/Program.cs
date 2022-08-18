53ab2efb0206ddba22761d57c1eded72c5e82a3a
ADK <artemdk7@gmail.com>
2022-08-18 20:33:58 +0700

    добавлено домашнее задание TASKs 19-21-23 final

--- dzTask19/Program.cs
+++ dzTask19/Program.cs
@@ -9,12 +9,17 @@ Console.Clear();
 Console.Write("Введите пятизначное целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int a = num / 10000;
-int num1 = num / 10000;
-int num2 = num / 1000;
-num2 = num2 % 10;
-int num4 = num % 100;
-num4 = num4 / 10;
-int num5 = num % 10;
-if (num1 == num5 && num2 == num4) Console.WriteLine("Число полиандром");
-else if (a == 0 || a > 10) Console.WriteLine("Число не пятизначное");
-else Console.WriteLine("Число не полиандром");
+int num1 = 0;
+int num2 = 0;
+int num4 = 0;
+int num5 = 0;
+if (a == 0 || a > 10) Console.WriteLine("Число не пятизначное");
+else
+{
+    num1 = num / 10000;
+    num2 = num / 1000 % 10;
+    num4 = num % 100 / 10;
+    num5 = num % 10;
+    if (num1 == num5 && num2 == num4) Console.WriteLine("Число полиандром");
+    else Console.WriteLine("Число не полиандром");
+}
