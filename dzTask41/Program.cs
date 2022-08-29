5e3ef4fd1b0f156fc9dbba6d695654327c2e9aa0
ADK <artemdk7@gmail.com>
2022-08-29 15:54:13 +0700

    домашнее задание практика-6

---
+++ dzTask41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// 0, 7, 8, -2, -2 -> 2 
+// 1, -7, 567, 89, 223-> 3 
+ 
+Console.Clear(); 
+Console.WriteLine("Задайте количество чисел для ввода: "); 
+int qty = Convert.ToInt32(Console.ReadLine()); 
+if (qty <= 0) 
+{ 
+    Console.WriteLine("Количество чисел для ввода, должно быть больше нуля"); 
+    return; 
+} 
+int[] Create(int size) 
+{ 
+    int[] arr = new int[size]; 
+    int j = 1; 
+    for (int i = 0; i < size; i++) 
+    { 
+        Console.WriteLine($"Введите число {j}: "); 
+        arr[i] = Convert.ToInt32(Console.ReadLine()); 
+        j++;
+    } 
+    return arr; 
+} 
+void Print(int[] array) 
+{ 
+ 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        if (i < array.Length - 1) Console.Write(array[i] + ", "); 
+        else Console.Write(array[i]); 
+    } 
+} 
+void Pos(int[] array) 
+{ 
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++) 
+        if (array[i] > 0) count++; 
+    Console.WriteLine(count); 
+} 
+int[] result = Create(qty); 
+Print(result); 
+Console.Write(" -> "); 
+Pos(result);
---
+++ dzTask43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. 
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) 
+ 
+Console.Clear(); 
+Console.WriteLine("Задайте b1: "); 
+double b1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Задайте к1: "); 
+double k1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Задайте b2: "); 
+double b2 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Задайте k2: "); 
+double k2 = Convert.ToInt32(Console.ReadLine()); 
+ 
+void Point (double b11, double k11, double b12, double k12 ) 
+{ 
+double x1 = k11 - k12; 
+double x2 = b12 - b11; 
+double x = x2/x1; 
+double y = k11*x+b11; 
+Console.WriteLine($"b1 = {b11}, k1 = {k11}, b2 = {b12}, k2 = {k12} -> ({x}; {y})"); 
+} 
+Point (b1,k1,b2,k2);
