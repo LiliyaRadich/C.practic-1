717ccc449212d9a7534713968ec9dbfe35734844
ADK <artemdk7@gmail.com>
2022-09-10 18:33:49 +0700

    домашнее задание практика-9

---
+++ DZ-Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// M = 1; N = 15 -> 120 
+// M = 4; N = 8. -> 30 
+Console.Clear();
+Console.Write("Введите 2 целых положительных чиcла(второе число, должно быть больше первого): ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < 0 || b <= 0 || b <= a) return;
+int sum = Stepen(a, b);
+Console.Write($"Cуммa натуральных элементов в промежутке от {a} до {b} -> {sum}");
+int Stepen(int dig1, int dig2)
+{
+    if (dig1 == dig2) return dig1;
+    return dig1 + Stepen(dig1 + 1, dig2); ;
+}
---
+++ DZ-Task68/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+// m = 3, n = 2 -> A(m,n) = 29 
+ 
+Console.Clear(); 
+Console.Write("Введите 2 целых положительных: "); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+int sum = Akkerman(a, b); 
+Console.Write($"m={a}, n={b} -> A({a},{b}) = {sum}"); 
+int Akkerman(int n, int m) 
+{ 
+    if (n == 0) return m + 1; 
+    else 
+    if (n != 0 && m == 0) return Akkerman(n - 1, 1); 
+    else return Akkerman(n - 1, Akkerman(n, m - 1)); 
+}
